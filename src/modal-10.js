7002947a5e45e7ff8e911b86d66d38af640d8a24
Ihor Kotliarevskyi <jagor0707@gmail.com>
2025-07-26 23:33:09 +0300

    add click backdrope done

--- src/modal-10.js
+++ src/modal-10.js
@@ -2,11 +2,14 @@
     const refs = {
       openModalBtn: document.querySelector("[data-modal-open-10]"),
       closeModalBtn: document.querySelector("[data-modal-close-10]"),
+      closeModalBdrp: document.querySelector("[data-modal-backdrope-close-10]"),
       modal: document.querySelector("[data-modal-10]"),
     };
   
     refs.openModalBtn.addEventListener("click", toggleModal);
     refs.closeModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBdrp.addEventListener("click", toggleModal);
+
   
     function toggleModal() {
       refs.modal.classList.toggle("is-hidden");
